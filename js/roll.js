77aa5d724f2f3cf0761cc812ac9885cbefb3a3e2
alebusi <allebusi@gmail.com>
2018-05-22 13:44:36 +0200

    Update roll.js

--- js/roll.js
+++ js/roll.js
@@ -135,6 +135,8 @@ window.addEventListener( 'DOMContentLoaded', function () {
     }
 
     buttonRoolDice.addEventListener( 'click', rollDice, false );
+	var snd = new Audio("success.wav");
+	snd.play();
 
 }, false);
 
